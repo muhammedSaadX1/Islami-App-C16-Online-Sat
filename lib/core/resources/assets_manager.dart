bbc62b542fd450c97485b6137b7ef48abeebbee7
Alaa Ahmed <alaaalprol@gmail.com>
2025-09-14 11:02:28 +0300

    finished sebha tab

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -11,6 +11,12 @@ abstract class ImageAssets {
   static const String hadithTabBg = "$base/hadith_tab_bg.png";
   static const String hadithCardBgImage = "$base/hadith_card_bg_image.png";
   static const String hadithCardBottomImage = "$base/hadith_card_bottom_image.png";
+  static const String sebhaTabBgImage = "$base/sebha_tab_bg_image.png";
+  static const String sebhaImage = "$base/sebha_image (1).png";
+  static const String radioTabBgImage = "$base/radio_tab_bg_image.png";
+  static const String timeTabBgImage = "$base/time_tab_bg_image.png";
+
+
 }
 
 abstract class IconAssets {
--- lib/features/main_layout/tasbeh/tasbeh_tab.dart
+++ lib/features/main_layout/tasbeh/tasbeh_tab.dart
@@ -1,12 +1,104 @@
-import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class TasbehTab extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:islami_app_online_sat/core/extensions/context_extensions.dart';
+import 'package:islami_app_online_sat/core/resources/assets_manager.dart';
+import 'package:islami_app_online_sat/core/resources/colors_manager.dart';
+
+class TasbehTab extends StatefulWidget {
   const TasbehTab({super.key});
+
+  @override
+  State<TasbehTab> createState() => _TasbehTabState();
+}
+
+class _TasbehTabState extends State<TasbehTab> {
+  int counter = 0;
+  double rotationAngle = 0.0;
+  int index = 0;
+  List<String> zeqrItem = [
+    "سبحان الله",
+    "الحمد لله",
+    "لا اله الا الله",
+    "الله اكبر",
+
+  ];
+  void incrementCounter() {
+    counter++;
+    rotationAngle += math.pi / 4;
+    if (counter == 33) {
+      index++;
+      counter = 0;
+      rotationAngle = 0.0;
+    }
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blueAccent,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(ImageAssets.sebhaTabBgImage)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(ImageAssets.islamiLogo),
+          SizedBox(height: context.getHeight * .01),
+          Center(
+            child: Text(
+              "سَبِّحِ اسْمَ رَبِّكَ الأعلى ",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: ColorsManager.white,
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: InkWell(
+                    onTap: incrementCounter,
+                    child: Transform.rotate(
+                      alignment: Alignment.center,
+                      angle: rotationAngle,
+                      child: Image.asset(ImageAssets.sebhaImage),
+                    ),
+                  ),
+                ),
+                Text(
+                  zeqrItem[index % zeqrItem.length],
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: ColorsManager.white,
+                  ),
+                ),
+
+                Positioned(
+                  bottom: context.getHeight * .16,
+                  child: Text(
+                    "$counter",
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+
+                      color: ColorsManager.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
